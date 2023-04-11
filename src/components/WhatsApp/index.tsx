c4da8af082eb9860218d3def61cf0f05ae745d1f
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-04-11 10:37:54 -0300

    ajusting img

--- src/components/WhatsApp/index.tsx
+++ src/components/WhatsApp/index.tsx
@@ -5,13 +5,15 @@ import styles from "./styles.module.scss";
 export default function WhatsApp() {
   return (
     <>
-      <section className={styles.whatsApp}>
-        <a
-          rel="noreferrer"
-          href="https://api.whatsapp.com/send?phone=5511957978876&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20de%20um%20site!"
-        >
-          <WhatsAnimation />
-        </a>
+      <section className={styles.sectionWhats}>
+        <div className={styles.whatsApp}>
+          <a
+            rel="noreferrer"
+            href="https://api.whatsapp.com/send?phone=5511957978876&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20de%20um%20site!"
+          >
+            <WhatsAnimation />
+          </a>
+        </div>
       </section>
     </>
   );
