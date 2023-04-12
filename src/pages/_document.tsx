44a384ce6ff5f5fdc7144d2d4ad791704a9d0ffa
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-04-12 11:35:42 -0300

    ajusting tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,13 @@ class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          {/* Início da tag do Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-M4DXC7H');`,
+            }}
+          />
+          {/* Fim da tag do Google Tag Manager */}
           <meta
             name="description"
             content="Criação de sites profissionais e personalizados para empresas. Transforme a presença digital da sua empresa com a ajuda do Rei dos Sites."
@@ -15,13 +22,20 @@ class MyDocument extends Document {
             content="Criação de Sites Profissionais - O Rei dos Sites"
           ></meta>
           <meta property="og:description" content="O Rei dos Sites:"></meta>
-          <meta
-            property="og:image:alt"
-            content="Thumbnail image for My Website"
-          />
+          <meta property="og:image:alt" content="O Rei do Sites" />
           <title>Criação de Sites Profissionais - O Rei dos Sites</title>
         </Head>
         <body>
+          {/* Início da tag do Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M4DXC7H"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          {/* Fim da tag do Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
